fdb6f4a1cd4f4d9b87eec3d97b4a6a4d72306e98
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-03-25 15:41:43 +0530

    ff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
